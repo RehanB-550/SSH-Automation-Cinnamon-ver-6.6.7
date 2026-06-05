f0440e8449f8e88d3880aad9f9f86ca03203d635
Rehan Bhatti <cs.bhatti288@gmail.com>
2026-06-06 00:29:53 +0500

    Add SSH server installation script
    Install OpenSSH server and related packages.

---
+++ SSH Server setup/ssh_install.sh
@@ -0,0 +1,32 @@
+pseudo@IdeaPad-Gaming-3-15ARH05:~$ sudo apt install openssh-server -y
+Reading package lists... Done
+Building dependency tree... Done
+Reading state information... Done
+The following additional packages will be installed:
+  ncurses-term openssh-client openssh-sftp-server ssh-import-id
+Suggested packages:
+  keychain libpam-ssh monkeysphere ssh-askpass molly-guard
+The following NEW packages will be installed:
+  ncurses-term openssh-server openssh-sftp-server ssh-import-id
+The following packages will be upgraded:
+  openssh-client
+1 upgraded, 4 newly installed, 0 to remove and 161 not upgraded.
+Need to get 1,739 kB of archives.
+After this operation, 6,747 kB of additional disk space will be used.
+Get:1 http://archive.ubuntu.com/ubuntu noble-updates/main amd64 openssh-client amd64 1:9.6p1-3ubuntu13.16 [907 kB]
+Get:2 http://archive.ubuntu.com/ubuntu noble-updates/main amd64 openssh-sftp-server amd64 1:9.6p1-3ubuntu13.16 [37.3 kB]
+Get:3 http://archive.ubuntu.com/ubuntu noble-updates/main amd64 openssh-server amd64 1:9.6p1-3ubuntu13.16 [510 kB]
+Get:4 http://archive.ubuntu.com/ubuntu noble/main amd64 ncurses-term all 6.4+20240113-1ubuntu2 [275 kB]
+Get:5 http://archive.ubuntu.com/ubuntu noble-updates/main amd64 ssh-import-id all 5.11-0ubuntu2.24.04.1 [10.1 kB]
+Fetched 1,739 kB in 3s (570 kB/s)
+Preconfiguring packages ...
+(Reading database ... 516891 files and directories currently installed.)
+Preparing to unpack .../openssh-client_1%3a9.6p1-3ubuntu13.16_amd64.deb ...
+Unpacking openssh-client (1:9.6p1-3ubuntu13.16) over (1:9.6p1-3ubuntu13.15) ...
+Selecting previously unselected package openssh-sftp-server.
+Preparing to unpack .../openssh-sftp-server_1%3a9.6p1-3ubuntu13.16_amd64.deb ...
+Unpacking openssh-sftp-server (1:9.6p1-3ubuntu13.16) ...
+Selecting previously unselected package openssh-server.
+Preparing to unpack .../openssh-server_1%3a9.6p1-3ubuntu13.16_amd64.deb ...
+Unpacking openssh-server (1:9.6p1-3ubuntu13.16) ...
+Progress: [ 29%] [###################...............................................]
